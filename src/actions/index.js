afae6339e55e73aecf7bd53610cfbce4f416dac4
Qu4k3r <neves0007@gmail.com>
2021-03-28 14:54:31 -0300

    Exercise async redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import fetchRandomCat from "../services";
+
 export const REQUEST_API = 'REQUEST_API';
 export const GET_PICTURE = 'GET_PICTURE';
 
@@ -7,4 +9,13 @@ export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
 export function fetchAPI() {
   // Desenvolva aqui o código da action assíncrona
+  return async (dispatch) => {
+    try {
+      dispatch(requestAPI());
+      const randomCatImg = await fetchRandomCat();
+      dispatch(getPicture(randomCatImg));
+    } catch(e) {
+      console.error(e);
+    }
+  }
 }
